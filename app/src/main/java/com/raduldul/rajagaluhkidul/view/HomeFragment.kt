50e49d6bcdcdf8e2118bd9e731db5ae033eb9f40
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-07-16 13:45:43 +0700

    adding layout welcome1

---
+++ app/src/main/java/com/raduldul/rajagaluhkidul/view/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.raduldul.rajagaluhkidul.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.raduldul.rajagaluhkidul.R
+import com.raduldul.rajagaluhkidul.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding : FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
