4b752374140b0eac11692d268336609c556fd8a4
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-08-02 21:52:40 +0700

    adding fragments detail umkm

---
+++ app/src/main/java/com/raduldul/rajagaluhkidul/view/DetailUmkmFragment.kt
@@ -0,0 +1,25 @@
+package com.raduldul.rajagaluhkidul.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.raduldul.rajagaluhkidul.R
+import com.raduldul.rajagaluhkidul.databinding.FragmentDetailUmkmBinding
+
+
+class DetailUmkmFragment : Fragment() {
+    private lateinit var binding: FragmentDetailUmkmBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailUmkmBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
